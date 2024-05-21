0035680695ce1f104dcce48495d3ea9ef79174af
Drew <drew.zukerman@fiscalnote.com>
2024-05-21 15:54:05 -0400

    inputField component, testing

--- my-app/src/components/InputField.tsx
+++ my-app/src/components/InputField.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './styles.css'
 
-const InputField = () => {
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField = ({todo, setTodo}: Props) => {
     return (
         <form className="input">
-            <input type="input" placeholder="enter a task" className="input__box"/>
-            <button className="input__submit" type="submit">Go</button>
+            <input type="input"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+            placeholder="enter a task" className="input__box"
+            />
+            <button className="input_submit" type="submit">Go</button>
         </form>
     )
 }
