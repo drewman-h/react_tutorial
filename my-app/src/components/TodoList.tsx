6f616acdc1383de8350d6ce4cb978d248835218a
Drew <drew.zukerman@fiscalnote.com>
2024-05-23 00:16:04 -0400

    more WIP on drag and drop, searching for very frustrating bug

--- my-app/src/components/TodoList.tsx
+++ my-app/src/components/TodoList.tsx
@@ -11,7 +11,7 @@ interface Props {
     setCompletedTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const TodoList: React.FC<Props> = ({todos, setTodos}: Props) => {
+const TodoList: React.FC<Props> = ({todos, setTodos, completedTodos, setCompletedTodos}: Props) => {
     return (
         <div className="container">
             <Droppable droppableId="TodosList">
@@ -23,30 +23,42 @@ const TodoList: React.FC<Props> = ({todos, setTodos}: Props) => {
                             {...provided.droppableProps}
                         >
                             <span className="todos__heading">Active Tasks</span>
-                            {todos.map((todo) => (
+                            {todos.map((todo, index) => (
                                 <SingleTodo
+                                    index={index}
                                     todo={todo}
                                     todos={todos}
                                     key={todo.id}
                                     setTodos={setTodos}
                                 />
                             ))}
+                            {provided.placeholder}
                         </div>
                     )
                 }
             </Droppable>
             <Droppable droppableId="todosRemove">
-                <div className="todos remove">
-                    <span className="todos__heading">Completed Tasks</span>
-                        {todos.map((todo) => (
-                            <SingleTodo
-                                todo={todo}
-                                todos={todos}
-                                key={todo.id}
-                                setTodos={setTodos}
-                            />
-                    ))}
-                </div>
+                {
+                    (provided) => (
+                        <div
+                            className="todos remove"
+                            ref={provided.innerRef}
+                            {...provided.droppableProps}
+                        >
+                            <span className="todos__heading">Completed Tasks</span>
+                                {completedTodos.map((todo, index) => (
+                                    <SingleTodo
+                                        index={index}
+                                        todo={todo}
+                                        todos={completedTodos}
+                                        key={todo.id}
+                                        setTodos={setCompletedTodos}
+                                    />
+                            ))}
+                            {provided.placeholder}
+                        </div>
+                    )
+                }
             </Droppable>
         </div>
     );
