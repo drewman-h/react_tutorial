a51f0754421ba967822076ed996e2888afb115fa
Drew <drew.zukerman@fiscalnote.com>
2024-05-22 21:58:27 -0400

    WIP: drag and drop

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
-import InputField from "./components/InputField"
-import TodoList from "./components/TodoList"
-import { Todo } from "./model"
+import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
+import { Todo } from "./model";
+import { DragDropContext} from 'react-beautiful-dnd';
 
 const App: React.FC = () => {
 
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
@@ -21,11 +23,18 @@ const App: React.FC = () => {
   console.log(todos);
 
   return (
-    <div className="App">
-      <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      <TodoList todos={todos} setTodos={setTodos} />
-    </div>
+    <DragDropContext onDragEnd={() => {}}>
+      <div className="App">
+        <span className="heading">Taskify</span>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+        <TodoList
+        todos={todos}
+        setTodos={setTodos}
+        completedTodos={completedTodos}
+        setCompletedTodos={setCompletedTodos}
+        />
+      </div>
+    </DragDropContext>
   );
 }
 
--- my-app/src/components/TodoList.tsx
+++ my-app/src/components/TodoList.tsx
@@ -2,23 +2,54 @@ import React from 'react';
 import { Todo } from '../model';
 import './styles.css';
 import SingleTodo from './SingleTodo';
+import { Droppable } from 'react-beautiful-dnd';
 
 interface Props {
     todos: Todo[];
     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+    completedTodos: Todo[];
+    setCompletedTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
 const TodoList: React.FC<Props> = ({todos, setTodos}: Props) => {
-    return <div className="todos">
-            {todos.map((todo) => (
-                <SingleTodo
-                todo={todo}
-                todos={todos}
-                key={todo.id}
-                setTodos={setTodos}
-                />
-            ))}
+    return (
+        <div className="container">
+            <Droppable droppableId="TodosList">
+                {
+                    (provided) => (
+                        <div
+                            className="todos"
+                            ref={provided.innerRef}
+                            {...provided.droppableProps}
+                        >
+                            <span className="todos__heading">Active Tasks</span>
+                            {todos.map((todo) => (
+                                <SingleTodo
+                                    todo={todo}
+                                    todos={todos}
+                                    key={todo.id}
+                                    setTodos={setTodos}
+                                />
+                            ))}
+                        </div>
+                    )
+                }
+            </Droppable>
+            <Droppable droppableId="todosRemove">
+                <div className="todos remove">
+                    <span className="todos__heading">Completed Tasks</span>
+                        {todos.map((todo) => (
+                            <SingleTodo
+                                todo={todo}
+                                todos={todos}
+                                key={todo.id}
+                                setTodos={setTodos}
+                            />
+                    ))}
+                </div>
+            </Droppable>
         </div>
-}
+    );
+};
 
 export default TodoList;
